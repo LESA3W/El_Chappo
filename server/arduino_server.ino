d985c716d344333d96e5d671b636444d42e986aa
Paul Chenon <78734263+LESA3W@users.noreply.github.com>
2026-03-02 16:04:25 -0800

    Update comments for clarity in arduino_server.ino

--- server/arduino_server.ino
+++ server/arduino_server.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-SoftwareSerial bt(2, 3); // RX, TX pour HC-05
+SoftwareSerial bt(2, 3); // RX, TX pr HC-05
 
 void setup() {
   Serial.begin(9600);    // liaison avec le PC
@@ -9,6 +9,6 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     char c = Serial.read();
-    bt.write(c); // transmet au HC-05
+    bt.write(c); // transmission pr HC-05
   }
 }
